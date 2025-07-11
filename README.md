6923a31368788356cf846a5520d30d91fd9cd3fd
TahaMucasiroglu <tahamucasiroglu@gmail.com>
2025-07-11 17:56:11 +0300

    Cv için ayrı mvc açıldı sub domain olacağı için. Cv için veritabanı taloları eklenecek örnek sociallink eklendi

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ logger manager program alışınca geçir test et
 
 ratelimiter test et
 
-
+cv sitesinde verileri cookie içine al oradan sun. yani sürekli f5 ile api zorlanmasın
 
 
 
---
+++ TahaMucasirogluBlog/Presentation/TahaMucasirogluBlog.Presentation.API/Controllers/Concrete/SocialLinkController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TahaMucasirogluBlog.Presentation.API.Controllers.Concrete
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialLinkController : ControllerBase
+    {
+    }
+}
